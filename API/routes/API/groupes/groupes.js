6d4622e96fbf11922a4408400d99d2c343c56cfb
carinecroteau <croteau.carine@carrefour.cegepvicto.ca>
2018-04-08 14:46:24 -0400

    first commit

---
+++ API/routes/API/groupes/groupes.js
@@ -0,0 +1,19 @@
+/**
+ * Module de routes API créé par Jean-Sébastien Lemelin
+ * @type {*|createApplication}
+ */
+var express = require('express');
+var router = express.Router();
+var Groupe = require('../../../models/groupe');
+
+router.post('/', function(req, res, next) {
+    var groupe = new Groupe({
+        nom:'Apprendrsdfasdfe',
+        actif:1
+    });
+    groupe.save();
+});
+
+
+
+module.exports = router;
